6c2a88449e27d41724844ef2b9a227f6131036d6
amitKrishh1 <akumar279182@gmail.com>
2023-08-07 21:57:08 +0530

    notification

---
+++ lib/view/notification/notificationView.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flygrs/Utils/res/colors.dart';
+import 'package:flygrs/res/components/HeaderBack.dart';
+
+class NotificationView extends StatefulWidget {
+  const NotificationView({super.key});
+
+  @override
+  State<NotificationView> createState() => _NotificationViewState();
+}
+
+class _NotificationViewState extends State<NotificationView> {
+  final List<String> entries = <String>['Payment Method', 'History', 'Invite Friends', 'Settings', 'Logout'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            color: AppColors.primaryBackgroundColor,
+            height: 210,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HeaderBack(whichBack: 'whiteBack',),
+                Padding(
+                  padding: const EdgeInsets.only(left: 27, top: 35),
+                  child: Text(
+                    'Notification',
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w900,
+                        color: AppColors.white),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height-210 ,
+            color: AppColors.secondaryBackgroundColor,
+            child: ListView.separated(
+                padding: EdgeInsets.only(top: 10),
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.black,
+                ),
+                itemCount: entries.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: EdgeInsets.only(left: 28, right: 28, top: 19, bottom: 19),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${entries[index]}',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: AppColors.black),
+                        ),
+                        SizedBox(height: 5,),
+                        Text(
+                          'Your booking #1234 has been suc...',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: AppColors.black),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
